604017560bee3bb73970b227b7001ed5a83fa581
Yana Sidneva <yan-zublenk2008@narod.ru>
2024-03-30 13:47:29 +0100

    feat: implement random adding 100 cars

--- project/src/components/createAddPart/createBtnPart.ts
+++ project/src/components/createAddPart/createBtnPart.ts
@@ -1,11 +1,28 @@
 import { createButton } from "../createButtons/createBtn";
+import { carNames } from "../../const/carNames";
+import GarageApi from "../../api/garageApi";
 
 export function createBtnContainer(): HTMLElement {
     const btnContainer = document.createElement('div');
     btnContainer.classList.add('btn-block');
     const racebtn = createButton('race');
     const resetbtn = createButton('reset');
-    const generate = createButton('generate cars');
+    const garageApi = new GarageApi(process.env.API_URL!, {});
+    const generate = createButton('generate cars', e => {
+        for (let i = 1; i <= 100; i++) {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            let randomColor = 'rgb(' + r + ',' + g + ',' + b + ')'
+            let randomName = carNames[Math.floor(Math.random() * carNames.length)];
+            garageApi.createCar({
+                color: randomColor,
+                name: randomName
+            }, render => {
+
+            })
+        }
+    });
     btnContainer.appendChild(racebtn);
     btnContainer.appendChild(resetbtn);
     btnContainer.appendChild(generate);
